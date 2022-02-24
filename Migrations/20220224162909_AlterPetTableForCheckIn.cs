2581127424a7b76c7deafaccbfea45fb09a5b177
aaron-r-rogers <rogers.aaron.r@gmail.com>
2022-02-24 12:00:04 -0600

    PUT api/pets/id/checkin endpoint created and model/migration updated for database; front end throws 405 error

---
+++ Migrations/20220224162909_AlterPetTableForCheckIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class AlterPetTableForCheckIn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "checkedInAt",
+                table: "Pets",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "checkedInAt",
+                table: "Pets");
+        }
+    }
+}
